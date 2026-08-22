c6c21adc59b0eec29090850b9cf2a8194b5fee3c
Abolfazl-Edalati <a.g.e.tech@outlook.com>
2026-08-22 18:09:22 +0330

    Build gift component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import Carousel from "./components/Carousel";
 import Collections from "./components/Collections";
+import Gift from "./components/Gift";
 import HeroSection from "./components/HeroSection";
 import Inspiration from "./components/Inspiration";
 import NavBar from "./components/NavBar";
@@ -8,13 +9,14 @@ import Story from "./components/Story";
 
 function App() {
   return (
-    <div data-theme="nord">
+    <div data-theme="" className="mb-20">
       <NavBar />
       <HeroSection />
       <Story />
       <Collections />
       <Carousel />
       <Inspiration />
+      <Gift />
     </div>
   );
 }
